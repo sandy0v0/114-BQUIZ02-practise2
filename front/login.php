033ce71fadd0fd4f192a51b9be0d862416473bfc
sandy0v0 <be0324ss@gmail.com>
2025-03-10 14:35:27 +0800

    建立會員登入頁面

---
+++ front/login.php
@@ -0,0 +1,21 @@
+<fieldset style="width: 50%; margin:auto;">
+    <legend>會員登入</legend>
+    <table>
+        <tr>
+            <td class="clo" style="width: 150px;">帳號</td>
+            <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td class="clo" style="width: 150px;">密碼</td>
+            <td><input type="password" name="pw" id="pw"></td>
+        </tr>
+        <tr>
+            <td>
+            <input type="submit" value="登入">
+            <input type="reset" value="清除">
+            </td>
+            <td></td>
+        </tr>
+    </table>
+
+</fieldset>
