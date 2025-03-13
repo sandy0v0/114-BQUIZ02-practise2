638399ebe6dff591417cf1bea90373def37c4819
sandy0v0 <be0324ss@gmail.com>
2025-03-13 16:10:09 +0800

    建立會員登入頁面+忘記密碼註冊+會員註冊功能+會員登入功能+前後台登入歡迎及登出功能

--- admin.php
+++ admin.php
@@ -39,10 +39,16 @@
 						<marquee>請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地！詳見最新文章</marquee>
 					</div>
                 	<span style="width:23%; display:inline-block;">
-                    	                    	<a href="?do=login">會員登入</a>
-                    	                    </span>
-                    	<div class="">
-                		                        </div>
+						<?php if(!isset($_SESSION['user'])):?>
+                    	    <a href="index.php?do=login">會員登入</a>
+                    	<?php else:?>
+							歡迎,<?=$_SESSION['user'];?>
+							<?php if($_SESSION['user']=='admin'):?>                    
+								<br><button onclick="location.href='admin.php'">管理</button>|
+							<?php endif;?>
+							<button onclick="logout()">登出</button>
+						<?php endif;?>
+					</span>
                 </div>
 			<?php
 
--- front/login.php
+++ front/login.php
@@ -11,8 +11,8 @@
         </tr>
         <tr>
             <td>
-            <input type="submit" value="登入">
-            <input type="reset" value="清除">
+            <input type="submit" value="登入" onclick="login()">
+            <input type="reset" value="清除" onclick="resetForm()">
             </td>
             <td>
                 <a href="?do=forgot">忘記密碼</a>｜
@@ -22,3 +22,47 @@
     </table>
 
 </fieldset>
+
+
+<script>
+
+function login(){
+    let user={
+    acc:$("#acc").val(),
+    pw:$("#pw").val(),
+
+    }
+
+
+    $.get("./api/chk_acc.php",{acc:user.acc},(res)=>{
+        if(parseInt(res)==0){
+            alert("查無帳號")
+            resetForm()
+        }else{
+            $.post("./api/chk_pw.php",user,(res)=>{
+                // console.log("login=>",res)
+                if(parseInt(res)==1){
+                    if(user.acc=='admin'){
+                        location.href='admin.php';
+                    }else{    
+                        location.href='index.php';
+                    }
+                }else{
+                    alert("密碼錯誤")
+                    resetForm()
+                }
+            })
+        }      
+    })
+}
+
+
+
+function resetForm(){
+    $("#acc").val("")
+    $("#pw").val("")
+
+}
+
+
+</script>
