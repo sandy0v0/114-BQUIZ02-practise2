78cfd856fa5da19dae6db37e421b4ecdbe5b2df1
sandy0v0 <be0324ss@gmail.com>
2025-03-14 14:09:00 +0800

    完成找回密碼功能

---
+++ back/acc.php
@@ -0,0 +1,105 @@
+<fieldset style="width: 75%;margin:auto">
+    <legend>帳號管理</legend>
+
+
+<table>
+    <tr>
+        <td>帳號</td>
+        <td>密碼</td>
+        <td>刪除</td>
+    </tr>
+    <?php
+    $rows=$User->all();
+    foreach($rows as $row):?>
+    <tr>
+        <td></td>
+        <td></td>
+        <td>
+            <input type="checkbox" name="del[]" id="" value="">
+        </td>
+    </tr>
+    <?php endforeach;?>
+
+</table>
+
+<h2>新增會員</h2>
+
+   
+    <div style="color:red">
+        *請設定您要註冊的帳號及密碼(最長12個字元)
+    </div>
+    <table>
+        <tr>
+            <td class="clo" style="width: 220px;">
+                Step1:登入帳號
+            </td>
+            <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td class="clo">
+                Step2:登入密碼
+            </td>
+            <td><input type="password" name="pw" id="pw"></td>
+        </tr>
+        <tr>
+            <td class="clo">
+                Step3:再次確認密碼
+            </td>
+            <td><input type="password" name="pw2" id="pw2"></td>
+        </tr>
+        <tr>
+            <td class="clo">
+                Step4:信箱(忘記密碼時使用)
+            </td>
+            <td><input type="text" name="email" id="email"></td>
+        </tr>
+        <tr>
+            <td>
+                <input type="button" value="註冊" onclick='reg()'>
+                <input type="button" value="清除" onclick='resetForm()'>
+            </td>
+        </tr>
+    </table>
+
+</fieldset>
+
+<script>
+function reg(){
+    let user={
+    acc:$("#acc").val(),
+    pw:$("#pw").val(),
+    pw2:$("#pw2").val(),
+    email:$("#email").val(),
+
+    }
+    // console.log(user)
+
+    if(user.acc=="" || user.pw=="" ||user.pw2=="" ||user.email==""){
+        alert("不可空白");
+    }else if(user.pw!=user.pw2){
+        alert("密碼錯誤");
+    }else{
+        $.get("./api/chk_acc.php",{acc:user.acc},(res)=>{
+            if(parseInt(res)>0){
+                alert("帳號重複")
+            }else{
+                $.post("./api/reg.php",user,(res)=>{
+                    // if(parseInt(res)==1){
+                    //     alert("註冊完成，歡迎加入")
+                    // }
+                })
+            }      
+        })
+    }
+}
+
+
+function resetForm(){
+    $("#acc").val("")
+    $("#pw").val("")
+    $("#pw2").val("")
+    $("#email").val("")
+
+}
+
+</script>
