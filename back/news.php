a25b71fa581dca0774a0fced18fe7658290a33af
sandy0v0 <be0324ss@gmail.com>
2025-03-14 16:29:54 +0800

    建立news資料表8筆資料+最新文章管理頁面建構+列表顯示及分頁功能

---
+++ back/news.php
@@ -0,0 +1,55 @@
+<fieldset style="width: 85%;magin:auto;">
+    <legend>最新文章管理</legend>
+<table class="ct" style="width: 100%;">
+    <tr>
+        <td>編號</td>
+        <td width="50%">標題</td>
+        <td>顯示</td>
+        <td>刪除</td>
+    </tr>
+    <?php
+    $total=$News->count();
+    $div=3;
+    $pages=ceil($total/$div);
+    $now=$_GET['p']??1;
+    $start=($now-1)*$div;
+
+    $rows=$News->all(" Limit $start,$div");
+    foreach($rows as $idx=>$row):
+    ?>
+    <tr>
+        <td><?=$start+$idx+1;?></td>
+        <td><?=$row['title'];?></td>
+        <td>
+        <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?"checked":"";?>>
+    </td>
+        <td>
+        <input type="checkbox" name="sh[]" value="<?=$row['id'];?>">
+        </td>
+    </tr>
+<?php endforeach;?>
+</table>
+
+<div class="ct">
+    <?php
+
+        if(($now-1)>0){
+            echo "<a href='?do=news&p".($now-1)."'> &lt;</a>";
+        }
+
+        for($i=1;$i<=$pages;$i++){
+            $size=($i==$now)?"24px":"16px";
+            echo "<a href='?do=news&p=$i' style='font-size:$size'> $i </a>";
+        }
+
+        if(($now+1)<=$pages){
+            echo "<a href='?do=news&p".($now+1)."'> &gt;</a>";
+        }
+
+?>
+</div>
+
+
+
+</fieldset>
+
