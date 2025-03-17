9d3997fba1f5e0a037dc33ed34a69e10bd05a1b9
sandy0v0 <be0324ss@gmail.com>
2025-03-17 10:41:25 +0800

    完成更換導覽文字及取得文章列表功能

--- front/po.php
+++ front/po.php
@@ -3,7 +3,7 @@
 </div>
 
 <style>
-    .type{
+    .type, .list-item{
         display: block;
         margin: 10px 0;
     }
@@ -11,13 +11,30 @@
 
 <fieldset style="width: 150px; display: inline-block;vertical-align: top;">
     <legend>分類網誌</legend>
-    <a href="javascript:getList(1)" class='type'>健康新知</a>
-    <a href="javascript:getList(2)" class='type'>菸害防治</a>
-    <a href="javascript:getList(3)" class='type'>癌症防治</a>
-    <a href="javascript:getList(4)" class='type'>慢性病防治</a>
+    <a href="#" class='type' data-type='1'>健康新知</a>
+    <a href="#" class='type' data-type='2'>菸害防治</a>
+    <a href="#" class='type' data-type='3'>癌症防治</a>
+    <a href="#" class='type' data-type='4'>慢性病防治</a>
 </fieldset>
 
 <fieldset style="width: 500px;display: inline-block;">
     <legend>文章列表</legend>
+    <div id="postList"></div>
 </fieldset>
 
+<script>
+
+getList(1)
+
+$(".type").on('click',function(){
+    $("#type").text($(this).text())
+
+    let type=$(this).data('type')
+    getList(type)
+})
+
+function getList(type){
+    $("#postList").load("./api/get_list.php",{type})
+}
+
+</script>
